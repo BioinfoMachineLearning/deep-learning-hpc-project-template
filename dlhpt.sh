2c020caf3f5c979ceab7727c5e44e560c9f6db86
Alex Morehead <alex.morehead@gmail.com>
2021-01-07 13:00:29 -0600

    Revert to Wandb integration stage in branch

---
+++ dlhpt.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+####################### BSUB Headers #########################
+#BSUB -P bip198
+#BSUB -W 0:10
+#BSUB -nnodes 1
+#BSUB -q batch
+#BSUB -J train_mnist_model_with_pl
+#BSUB -o /gpfs/alpine/scratch/acmwhb/bip198/Repositories/Personal_Repositories/deep-learning-hpc-project-template/job%J.out
+#BSUB -e /gpfs/alpine/scratch/acmwhb/bip198/Repositories/Personal_Repositories/deep-learning-hpc-project-template/job%J.out
+###############################################################
+
+# Remote project path
+export PROJDIR=$MEMBERWORK/bip198/Repositories/Personal_Repositories/deep-learning-hpc-project-template
+
+# Configure Conda for BSUB script environment
+eval "$(conda shell.bash hook)"
+
+# Remote Conda environment
+conda activate "$PROJDIR"/venv
+
+# Run training script
+jsrun -bpacked:7 -r1 -g2 -a6 -c42 python3 "$PROJDIR"/project/lit_image_classifier.py --gpus 2 --max_epochs 5 --num_dataloader_workers 16 --learning_rate 1e-2
