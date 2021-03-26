5e7146f797f7eec1c011c673bd03269fbc757452
Alex Morehead <alex.morehead@gmail.com>
2021-03-26 13:58:58 -0500

    Add NVME alloc_flag

--- train.bash
+++ train.bash
@@ -7,6 +7,7 @@
 #BSUB -nnodes 2
 #BSUB -q batch-hm
 #BSUB -alloc_flags "gpumps"
+#BSUB -alloc_flags "NMVE"
 #BSUB -o /gpfs/alpine/scratch/acmwhb/bip198/Repositories/Personal_Repositories/deep-learning-hpc-project-template/job%J.out
 #BSUB -e /gpfs/alpine/scratch/acmwhb/bip198/Repositories/Personal_Repositories/deep-learning-hpc-project-template/job%J.out
 ###############################################################
