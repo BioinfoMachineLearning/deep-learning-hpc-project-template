968335571ccda22be1c6a12dca949c48120f726f
Alex Morehead <alex.morehead@gmail.com>
2021-07-23 13:21:42 -0500

    Export proxy URLs

--- train_nvme.bash
+++ train_nvme.bash
@@ -21,6 +21,14 @@ conda activate DLHPT
 # Configure OMP for PyTorch
 export OMP_PLACES=threads
 
+# Configure proxy access on compute nodes
+export all_proxy=socks://proxy.ccs.ornl.gov:3128/
+export ftp_proxy=ftp://proxy.ccs.ornl.gov:3128/
+export http_proxy=http://proxy.ccs.ornl.gov:3128/
+export https_proxy=https://proxy.ccs.ornl.gov:3128/
+export no_proxy='localhost,127.0.0.0/8,.ccs.ornl.gov,.ncrc.gov'
+export LC_ALL=en_US.utf8
+
 # Run training script
 cd "$PROJDIR"/project || exit
 
