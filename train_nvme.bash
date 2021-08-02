0a2ba0c212eb4de1396bd25a4fd7b98d0aa79031
Alex Morehead <alex.morehead@gmail.com>
2021-08-01 22:47:37 -0500

    Update HTTPS proxy URL in response to urllib3 change (for troubleshooting)

--- train_nvme.bash
+++ train_nvme.bash
@@ -25,7 +25,7 @@ export OMP_PLACES=threads
 export all_proxy=socks://proxy.ccs.ornl.gov:3128/
 export ftp_proxy=ftp://proxy.ccs.ornl.gov:3128/
 export http_proxy=http://proxy.ccs.ornl.gov:3128/
-export https_proxy=https://proxy.ccs.ornl.gov:3128/
+export https_proxy=http://proxy.ccs.ornl.gov:3128/
 export no_proxy='localhost,127.0.0.0/8,.ccs.ornl.gov,.ncrc.gov'
 export LC_ALL=en_US.utf8
 
